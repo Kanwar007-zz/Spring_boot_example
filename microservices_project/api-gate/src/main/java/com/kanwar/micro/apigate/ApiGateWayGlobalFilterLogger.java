65da06550c0bf7b48d3c8b9c30e2564bf00f4d51
Kanwar007 <vijayblp.kanwar@gmail.com>
2021-12-05 14:35:40 +0800

    golder fiter added

---
+++ microservices_project/api-gate/src/main/java/com/kanwar/micro/apigate/ApiGateWayGlobalFilterLogger.java
@@ -0,0 +1,24 @@
+package com.kanwar.micro.apigate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class ApiGateWayGlobalFilterLogger implements GlobalFilter {
+	Logger logger = LoggerFactory.getLogger(ApiGateWayGlobalFilterLogger.class);
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		// TODO Auto-generated method stub
+		
+		logger.info("Get Patch information -> {}"+exchange.getRequest().getPath());
+		return chain.filter(exchange);
+	}
+
+}
